b0eaf5bc1723391faf6716d28d35d0426021657d
PetarK20 <148004863+PetarK20@users.noreply.github.com>
2025-10-08 18:18:01 +0300

    Add files via upload

---
+++ zad.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char op;
+
+    std::cout << "Enter first num:  ";
+    std::cin >> num1;
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+    std::cout << "Enter second num: ";
+    std::cin >> num2;
+
+    double result;
+    switch(op) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/':
+            if(num2 == 0) {
+                std::cout << "Error: Division with 0!" << std::endl;
+                return 1;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            std::cout << "Invalid operator!" << std::endl;
+            return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
